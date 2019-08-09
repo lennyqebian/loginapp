32ca14c3ef2164a19b1e2e3821b32828512c1dd7
Leonardo Quevedo Diaz <jorgequevedo@cbit-online.com>
2019-08-09 10:37:59 -0500

    Firebase Config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyB66H4PiPHbcEF7ncdXB1Un9QgRj6orLvA',
+    authDomain: 'loginapp-489d1.firebaseapp.com',
+    databaseURL: 'https://loginapp-489d1.firebaseio.com',
+    projectId: 'loginapp-489d1',
+    storageBucket: 'loginapp-489d1.appspot.com',
+    messagingSenderId: '189270384828',
+    appId: '1:189270384828:web:ba16b0987bf1c345'
+  }
 };
 
 /*
